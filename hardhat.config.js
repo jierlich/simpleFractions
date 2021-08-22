ce068547a426c2a9f66af3b0f0a1e17c890b20be
jierlich <jierlich@umich.edu>
2021-08-22 14:12:12 +0200

    [DEV] initial hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.4",
+  }
+};
