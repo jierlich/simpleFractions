c997fb7c9e1bfc524adc0d7c520d3f9928d41b5d
jierlich <jierlich@umich.edu>
2021-08-25 00:37:48 +0200

    [DEV] add ropsten alchemy to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+const environment = require('./environment')
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${environment.alchemyAPIKey}`,
+      accounts: [environment.deployerPK]
+    },
+  },
   solidity: {
     version: "0.8.4",
   }
